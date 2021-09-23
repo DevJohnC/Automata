7c5e0ab31cc51d1e263b3527c41bffdaef4c9f49
John Carruthers <johnc@frag-labs.com>
2021-09-23 00:29:19 -0500

    Add more API overloads for different light switch representations.

--- Devices/DevicesClient/src/NetworkStateExtensions.cs
+++ Devices/DevicesClient/src/NetworkStateExtensions.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Automata.Client;
 using Automata.Client.Resources;
-using Automata.Kinds;
 
 namespace Automata.Devices
 {
--- Lights/LightsClient/src/StateClientExtensions.cs
+++ Lights/LightsClient/src/StateClientExtensions.cs
@@ -9,28 +9,74 @@ namespace LightsClient
     public static class StateClientExtensions
     {
         public static Task TurnOn(this AutomataNetwork network,
-            TrackingDeviceHandle<LightSwitch, LightSwitchState> lightSwitch)
+            ResourceDocument<LightSwitch> lightSwitch)
         {
             return network.ChangeState(
-                new ResourceDocument<LightSwitch>(lightSwitch.DeviceId, lightSwitch.Device),
+                lightSwitch,
                 new SetPowerState(LightSwitchPowerState.On));
+        }
+        
+        public static Task TurnOn(this AutomataNetwork network,
+            DeviceHandle<LightSwitch, LightSwitchState> lightSwitch)
+        {
+            return network.TurnOn(
+                new ResourceDocument<LightSwitch>(lightSwitch.DeviceId, lightSwitch.Device));
+        }
+        
+        public static Task TurnOn(this AutomataNetwork network,
+            TrackingDeviceHandle<LightSwitch, LightSwitchState> lightSwitch)
+        {
+            return network.TurnOn(
+                new ResourceDocument<LightSwitch>(lightSwitch.DeviceId, lightSwitch.Device));
+        }
+        
+        public static Task TurnOff(this AutomataNetwork network,
+            ResourceDocument<LightSwitch> lightSwitch)
+        {
+            return network.ChangeState(
+                lightSwitch,
+                new SetPowerState(LightSwitchPowerState.Off));
+        }
+        
+        public static Task TurnOff(this AutomataNetwork network,
+            DeviceHandle<LightSwitch, LightSwitchState> lightSwitch)
+        {
+            return network.TurnOff(
+                new ResourceDocument<LightSwitch>(lightSwitch.DeviceId, lightSwitch.Device));
         }
         
         public static Task TurnOff(this AutomataNetwork network,
             TrackingDeviceHandle<LightSwitch, LightSwitchState> lightSwitch)
         {
+            return network.TurnOff(
+                new ResourceDocument<LightSwitch>(lightSwitch.DeviceId, lightSwitch.Device));
+        }
+        
+        public static Task SetPowerLevel(this AutomataNetwork network,
+            ResourceDocument<LightSwitch> lightSwitch,
+            double powerLevel)
+        {
             return network.ChangeState(
+                lightSwitch,
+                new SetPowerLevel(powerLevel));
+        }
+        
+        public static Task SetPowerLevel(this AutomataNetwork network,
+            DeviceHandle<LightSwitch, LightSwitchState> lightSwitch,
+            double powerLevel)
+        {
+            return network.SetPowerLevel(
                 new ResourceDocument<LightSwitch>(lightSwitch.DeviceId, lightSwitch.Device),
-                new SetPowerState(LightSwitchPowerState.Off));
+                powerLevel);
         }
         
         public static Task SetPowerLevel(this AutomataNetwork network,
             TrackingDeviceHandle<LightSwitch, LightSwitchState> lightSwitch,
             double powerLevel)
         {
-            return network.ChangeState(
+            return network.SetPowerLevel(
                 new ResourceDocument<LightSwitch>(lightSwitch.DeviceId, lightSwitch.Device),
-                new SetPowerLevel(powerLevel));
+                powerLevel);
         }
     }
 }
